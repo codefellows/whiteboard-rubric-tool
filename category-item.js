3c63d1d2f059465d676d34ae6fa4c32c7331029c
Steve Geluso <stevegeluso@gmail.com>
2018-06-19 00:35:05 -0700

    showing max score to start instead of minning to zero

--- category-item.js
+++ category-item.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 
 export default class CategoryItem extends Component {
+  state = {
+    score: this.props.item.max
+  }
+
   render() {
     return <div>
       <label className="category-item">
         <input type="integer" min="0" max={this.props.item.max}
-               size="2" value={this.props.item.score} />
+               size="2" value={this.state.score} />
         {"/"} {this.props.item.max}
         {" "}{this.props.item.name}
       </label>
--- rubric-data.js
+++ rubric-data.js
@@ -2,37 +2,37 @@ export default [
   {
     name: 'Interpreted the question',
     items: [
-      { max: 2, score: 0, name: 'Asked Meaningful clarifying questions'},
-      { max: 2, score: 0, name: 'Identified inputs and outputs'},
-      { max: 2, score: 0, name: 'Visually illustrated the problem domain'},
-      { max: 4, score: 0, name: 'Identified correct structure, and algorithm'},
+      { max: 2, name: 'Asked Meaningful clarifying questions'},
+      { max: 2, name: 'Identified inputs and outputs'},
+      { max: 2, name: 'Visually illustrated the problem domain'},
+      { max: 4, name: 'Identified correct structure, and algorithm'},
     ]
   },
   {
     name: 'Solved the technical problem',
     items: [
-      { max: 4, score: 0, name: 'Presents essential pseudocode solution'},
-      { max: 3, score: 0, name: 'Final code was syntactically correct'},
-      { max: 3, score: 0, name: 'Final code was idiomatically correct'},
-      { max: 2, score: 0, name: 'Solution was the best possible option'},
+      { max: 4, name: 'Presents essential pseudocode solution'},
+      { max: 3, name: 'Final code was syntactically correct'},
+      { max: 3, name: 'Final code was idiomatically correct'},
+      { max: 2, name: 'Solution was the best possible option'},
     ]
   },
   {
     name: 'Analyzed the proposed solution',
     items: [
-      { max: 2, score: 0, name: 'Stepped through their solution'},
-      { max: 2, score: 0, name: 'Big-O time and space are analyzed'},
-      { max: 2, score: 0, name: 'Explain an approach to testing'},
+      { max: 2, name: 'Stepped through their solution'},
+      { max: 2, name: 'Big-O time and space are analyzed'},
+      { max: 2, name: 'Explain an approach to testing'},
     ]
   },
   {
     name: 'Communicated effectively throughout',
     items: [
-      { max: 2, score: 0, name: 'Verbalized their thought process'},
-      { max: 2, score: 0, name: 'Used correct terminology'},
-      { max: 2, score: 0, name: 'Was not overconfident (not listening to suggestions)'},
-      { max: 4, score: 0, name: 'Was not under-confident (unsure of known algorithm)'},
-      { max: 4, score: 0, name: 'Whiteboard was readable (penmanship and spacing)'},
+      { max: 2, name: 'Verbalized their thought process'},
+      { max: 2, name: 'Used correct terminology'},
+      { max: 2, name: 'Was not overconfident (not listening to suggestions)'},
+      { max: 4, name: 'Was not under-confident (unsure of known algorithm)'},
+      { max: 4, name: 'Whiteboard was readable (penmanship and spacing)'},
     ]
   },
 ]
